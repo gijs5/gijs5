123ac4032122d8ab5aededb72d215e07cbd9d1b8
gijs5 <gijs@gijs5.nl>
2015-07-08 18:44:18 +0200

    first bse

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo 'test2'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gijs Vijfhuizen</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+	<div class="container">
+		<h1>Gijs Vijfhuizen</h1>
+		<ul class="buttons">
+			<li><a href="mailto:gijs@gijs5.nl" class="fa fa-envelope-o fa-3x"></a></li>
+			<li><a href="https://nl.linkedin.com/pub/gijs-vijfhuizen/5b/455/119" class="fa fa-linkedin-square fa-3x"></a></li>
+		</ul>
+	</div>
+</body>
+
+</html>
