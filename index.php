4945be44bd583dd2a9d955326e18a6680597510f
gijs5 <gijs@gijs5.nl>
2016-01-10 17:47:01 +0100

    clearer

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>Gijs Vijfhuizen</title>
+<meta name="viewport" content="width=device-width" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
